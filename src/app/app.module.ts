ac5845587c0e68d49f81cd2d7c62c5bc7b2bdc7a
= <=>
2019-06-26 12:03:07 +0100

    apply basic material styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+
+import { MaterialModule } from './material.module';
+
 import { TradeListComponent } from './components/trade-list/trade-list.component';
 import { PricingTermListComponent } from './components/pricing-term-list/pricing-term-list.component';
 import { PricingContainerComponent } from './components/pricing-container/pricing-container.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TradeListComponent,
     PricingTermListComponent,
-    PricingContainerComponent
+    PricingContainerComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
