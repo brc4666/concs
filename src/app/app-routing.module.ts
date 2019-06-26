8363f2cf1e114ac545959869b7f55178943a054d
= <=>
2019-06-26 14:45:28 +0100

    add query params to get specific trade pricing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PricingContainerComponent } from './components/pricing-container/pricing-container.component';
+import { TradeListComponent } from './components/trade-list/trade-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: TradeListComponent },
+  { path: 'pricingTerms', component: PricingContainerComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/pricing-container/pricing-container.component.ts
+++ src/app/components/pricing-container/pricing-container.component.ts
@@ -6,6 +6,7 @@ import { ViewService } from 'src/app/shared/view.service';
 import { takeUntil } from 'rxjs/operators';
 import { IFieldDef } from 'src/app/models/_base';
 import { IPricingTermModel } from 'src/app/shared/pricing-term-models';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-pricing-container',
@@ -19,24 +20,28 @@ export class PricingContainerComponent implements OnInit, OnDestroy {
 
   private unsub: Subject<void> = new Subject<any>();
 
+  private queryParams;
+
   constructor(
+    private route: ActivatedRoute,
     private pricingTermService: PricingTermService,
     private dataService: DataService,
     private viewService: ViewService
   ) { }
 
   ngOnInit() {
+    this.queryParams = this.route.snapshot.queryParams;
     this.models = this.pricingTermService.getPricingTermModels();
     this.pricingTerms$ = this.getPricingTerms(this.models);
     this.fieldDefs$ = this.getFieldDefs(this.models);
     this.readFromDB();
   }
 
-  private getPricingTerms(models): Observable<any>[] {
+  private getPricingTerms(models): Observable<IPricingTermModel<any>>[] {
     return models.map(model => this.dataService.getObservable(model));
   }
 
-  private getFieldDefs(models): Observable<any>[] {
+  private getFieldDefs(models): Observable<IFieldDef[]>[] {
     return models.map(model => this.viewService.getFieldDefintions(model));
   }
 
@@ -50,7 +55,7 @@ export class PricingContainerComponent implements OnInit, OnDestroy {
   }
 
   private getReadSubs(models): Observable<any>[] {
-    return models.map(model => this.dataService.read(model));
+    return models.map(model => this.dataService.read(model, this.queryParams));
   }
 
   ngOnDestroy(): void {
